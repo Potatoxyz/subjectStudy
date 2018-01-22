a77080293e266bd05b06070412b8c9fb0ecb0828
PotatoGG <512006634@qq.com>
2018-01-22 18:33:54 +0800

    index

---
+++ js/index-rightBar-innerToggle.js
@@ -0,0 +1,24 @@
+var rightBarInner=function () {
+    var tab1content=$('#tab1-content');
+    var tab2content=$('#tab2-content');
+    var tab3content=$('#tab3-content');
+    var selectedTab=$("input[name='tab']");
+    $(selectedTab).click(function () {
+        var selectedTabValue=$(this).val();
+        if(selectedTabValue==='tab1'){
+            $(tab1content).css('left','0');
+            $(tab2content).css('left','100%');
+            $(tab3content).css('left','200%');
+        }
+        if(selectedTabValue==='tab2'){
+            $(tab1content).css('left','-100%');
+            $(tab2content).css('left','0');
+            $(tab3content).css('left','100%');
+        }
+        if(selectedTabValue==='tab3'){
+            $(tab1content).css('left','-200%');
+            $(tab2content).css('left','-100%');
+            $(tab3content).css('left','0');
+        }
+    })
+};
---
+++ js/index-toggleBar.js
@@ -0,0 +1,26 @@
+var toggleBar=function () {
+    var rightFun=$('#rightFun');
+    var isopenleftBar=false;
+    var leftBar=$('#leftBar');
+    var sideBar=$('#sideBar');
+    var closeRightFun=$('#closeRightFun');
+    var rightBar=$('#rightBar');
+    $(rightFun).click(function () {
+        $(rightBar).removeClass('toggleRightBar');
+    });
+    $(closeRightFun).click(function () {
+        $(rightBar).addClass('toggleRightBar');
+    });
+    $(leftBar).click(function () {
+        if(!isopenleftBar){
+            $(sideBar).addClass('toggleSideBar');
+            $(leftBar).addClass('bg-black');
+            isopenleftBar=true;
+        }
+        else{
+            $(sideBar).removeClass('toggleSideBar');
+            $(leftBar).removeClass('bg-black');
+            isopenleftBar=false;
+        }
+    })
+};
